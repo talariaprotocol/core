3bf9be5c13cdfa74ca6c74175b8f89259e02dce6
Martin Grabina <mgrabina@itba.edu.ar>
2024-09-07 20:23:11 -0300

    improved warn messages with links

--- packages/nextjs/app/early-access/page.tsx
+++ packages/nextjs/app/early-access/page.tsx
@@ -8,13 +8,32 @@ const EarlyAccessHomePage = () => {
       application="early-access"
       warningMessage={
         <p>
-          We had transactions pending indeterminately while testing this feature. <br></br>Please follow the discussion on{" "}
+          We had transactions pending indeterminately while testing this feature. <br></br>Please follow the discussion
+          on{" "}
           <a
             className="underline"
             href="https://discord.com/channels/1156486804661338162/1209591862596468786/1281634619565019218"
           >
-            MorphL2's Discord channel.
+            MorphL2's Discord channel
           </a>
+          , see the code in{" "}
+          <a
+            className="underline"
+            target="_blank"
+            href="https://github.com/mgrabina/commit/blob/dev/packages/hardhat/contracts/useCases/EarlyAccessCodes.sol"
+          >
+            GitHub
+          </a>
+          , the deployment on{" "}
+          <a
+            className="underline"
+            target="_blank"
+            href="https://explorer-holesky.morphl2.io/address/0xB808ffe40A4Add6DF866a22ace5AfCD1a782EcC0?tab=contract"
+          >
+            {" "}
+            explorer-holesky.morphl2.io
+          </a>
+          , contact us, or test the logic on any other chain.
         </p>
       }
       title="MorphL2 Early Access for Protocols"
