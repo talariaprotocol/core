82cdeb7ab96b787b14991d9ec2a489f40fea310f
Martin Grabina <mgrabina@itba.edu.ar>
2024-09-07 21:00:33 -0300

    change a bit texts

--- packages/nextjs/app/chiliz/page.tsx
+++ packages/nextjs/app/chiliz/page.tsx
@@ -21,7 +21,7 @@ const GiftcardHomePage = () => {
       title="Chiliz-Barcelona Token Distribution"
       logoSrc={BCNLogo}
       subTitle="
-        Since most football fans doesn't have a wallet ready yet, distributing BCN tokens to the club members is either centralized or impossible.
+        Since most football fans don't have a wallet ready yet, distributing BCN tokens to the club members is either centralized or impossible.
         Now, using TornadoCodes technology, the BCN team can distribute tokens to their users simply by sharing a code for future redemption with any wallet they choose.
         Cheap. Decentralized. Mathematically Secure. The future of fan tokens distribution is here.
         "
