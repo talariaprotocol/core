54eb6c7ca9fa2eb1e8f3be7998919af78090cf4d
Martin Grabina <mgrabina@itba.edu.ar>
2024-09-26 12:44:42 -0300

    change title

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -5,7 +5,7 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Commit Protocol",
+  title: "Talaria",
   description: "Descentralized access control for your smart contracts",
 });
 
